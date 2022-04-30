236699c1b668f16bdc68a4567b646122e498f2de
vidhyavishali kv <vidhyavishali.kv@gmail.com>
2022-04-30 21:57:21 +0200

    week2 day2 partial submit 2

---
+++ src/main/java/week2/day2/Buttons.java
@@ -0,0 +1,30 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Buttons {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.get("http://leafground.com/pages/Button.html");
+		String link = driver.findElement(By.id("home")).getAttribute("onclick");
+		System.out.println(link);
+		Point location = driver.findElement(By.id("position")).getLocation();
+		System.out.println(location.getX() + ", " + location.getY());
+		String color = driver.findElement(By.id("color")).getCssValue("background-color");
+		System.out.println(color);
+		Dimension size = driver.findElement(By.id("size")).getSize();
+		System.out.println(size.getHeight() + ", " + size.getWidth());
+
+		driver.close();
+
+	}
+
+}
--- src/main/java/week2/day2/EditFields.java
+++ src/main/java/week2/day2/EditFields.java
@@ -3,26 +3,32 @@ package week2.day2;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class EditFields {
-	
+
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
-		
-		
-	
+
 		driver.get("http://leafground.com/pages/Edit.html");
 		driver.findElement(By.id("email")).sendKeys("vidhyavishali.kv@gmail.com");
-		List<WebElement> findElements = driver.findElements(By.tagName("select"));
-		System.out.println(findElements.size());
+		WebElement appendElement = driver.findElement(By.xpath("//input[contains(@value,'Append')]"));
+		appendElement.sendKeys(appendElement.getText() + " Extra text" + Keys.TAB);
+
+		String text = driver.findElement(By.xpath("//label[contains(text(),'default text')]/following-sibling::input"))
+				.getAttribute("value");
+		System.out.println(text);
+		driver.findElement(By.xpath("//input[@value='Clear me!!']")).sendKeys("");
+		if (!driver.findElement(By.xpath("//label[contains(text(),'is disabled')]/following-sibling::input"))
+				.isEnabled())
+			System.out.println("Confirming the field is disabled");
 		
-				
-		
+		driver.close();
 	}
 
 }
